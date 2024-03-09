4e109442f383219199f474800e7e8226adc10c45
CASY82 <repaid11010@gmail.com>
2024-03-10 07:19:01 +0900

    add : 20240310 application.yml에 db 세팅 및 thymeleaf와 Vue 세팅 완료(테스트 페이지 생성)

--- src/main/java/com/eps/cisco/EpsApplication.java
+++ src/main/java/com/eps/cisco/EpsApplication.java
@@ -2,9 +2,8 @@ package com.eps.cisco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class EpsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/eps/cisco/presentation/view/ViewController.java
@@ -0,0 +1,16 @@
+package com.eps.cisco.presentation.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Thymeleaf 컨트롤 전용
+ */
+@Controller
+public class ViewController {
+	
+	@RequestMapping(value = "/main")
+	public String mainPage() {
+		return "main";
+	}
+}
