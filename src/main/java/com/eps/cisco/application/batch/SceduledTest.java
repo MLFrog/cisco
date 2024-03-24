3f3c666075b89ed6221216a07f5be27a4900b45f
CASY82 <repaid11010@gmail.com>
2024-03-25 01:04:11 +0900

    add : Scheduled 설정 및 Batch 테스트 클래스 추가(추후 테스트 클래스는 제거 예정)

---
+++ src/main/java/com/eps/cisco/application/batch/SceduledTest.java
@@ -0,0 +1,17 @@
+package com.eps.cisco.application.batch;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableScheduling
+@Service
+public class SceduledTest {
+	@Scheduled(cron = "0/5 * * * * ?")
+	public void scheduledCron() {
+		log.info("TestScheduledCron");
+	}
+}
