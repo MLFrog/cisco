abf3489907a03573ef47e130fa909b87e55ae964
CASY82 <repaid11010@gmail.com>
2024-03-31 22:50:58 +0900

    add : Domain Common 어노테이션 및 인터페이스 생성

---
+++ src/main/java/com/eps/cisco/domain/common/DomainEntity.java
@@ -0,0 +1,8 @@
+package com.eps.cisco.domain.common;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface DomainEntity {
+}
